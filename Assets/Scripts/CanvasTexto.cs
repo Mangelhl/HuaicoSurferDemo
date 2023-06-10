2ef070a1ccfe205706fdad0b0cf539ff1bb24896
Mangelhl <datcom221@gmail.com>
2023-06-09 19:15:14 -0500

    Ejecutable - Miguel Angel

---
+++ Assets/Scripts/CanvasTexto.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class CanvasTexto : MonoBehaviour
+{
+    public static CanvasTexto CT;
+    public TextMeshProUGUI EscenaGame;
+    public TextMeshProUGUI EscenaMuerte;
+    public TextMeshProUGUI Moneditas;
+    public GameManager GM;
+    public int PuntajeInGame;
+    public int monedas;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if(CanvasTexto.CT == null)
+        {
+            CanvasTexto.CT = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else if(CanvasTexto.CT != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       GM = FindObjectOfType<GameManager>();
+        EscenaGame = FindObjectOfType<TextMeshProUGUI>();
+        EscenaMuerte = FindObjectOfType<TextMeshProUGUI>();
+        GameObject objetoMonedas = GameObject.Find("Monedas");
+        if (objetoMonedas != null)
+        {
+            Moneditas = objetoMonedas.GetComponent<TextMeshProUGUI>();
+        }
+
+        Scene escenaActual = SceneManager.GetActiveScene();
+        if (escenaActual.buildIndex == 1)
+        {
+            PuntajeInGame = GM.Puntuacion;
+            EscenaGame.text = PuntajeInGame.ToString();
+        }
+        else if (escenaActual.buildIndex == 2)
+        {
+            EscenaMuerte.text = PuntajeInGame.ToString();
+        }
+
+
+        Moneditas.text = monedas.ToString();
+    }
+}
