2aa4b0b20538d02f5a2f8a35bd80c4a83934437b
JuanJoséFerreiraChavez <jjtrabajos159@gmail.com>
2023-06-23 22:48:33 -0500

    Arreglar Errores y Creditos Mejora
    ahora te lleva de creditos a menú automáticamente y el problema con el código de miguel es que ocultaba todo menos mi menú y hice código para que funcione cuando estas en el menú

--- Assets/ScripsBase/ScripJJ/IU/MejorasTienda.cs
+++ Assets/ScripsBase/ScripJJ/IU/MejorasTienda.cs
@@ -1,18 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Networking.Types;
 using UnityEngine.UI;
 public class MejorasTienda : MonoBehaviour
 {
     MonedasLucas ML;
-    static int Infra, Ambi, Prev;
     [SerializeField, Tooltip("Cuanto costara la mejora")] int Costo1, Costo2, Costo3;
    [SerializeField] GameObject[] Botones;
     [SerializeField] float MaximoV = 3;
+
+    private void Awake()
+    {
+        switch (PlayerPrefs.GetInt("Minfra", 0))
+        {
+            case 1:
+                Botones[0].SetActive(false);
+                break;
+            case 2:
+                Botones[0].SetActive(false);
+                Botones[1].SetActive(false);
+                break;
+            case 3:
+                Botones[0].SetActive(false);
+                Botones[1].SetActive(false);
+                Botones[2].SetActive(false);
+                break;
+        }
+        switch (PlayerPrefs.GetInt("Mambi", 0))
+        {
+            case 1:
+                Botones[6].SetActive(false);
+                break;
+            case 2:
+                Botones[6].SetActive(false);
+                Botones[7].SetActive(false);
+                break;
+            case 3:
+                Botones[6].SetActive(false);
+                Botones[7].SetActive(false);
+                Botones[8].SetActive(false);
+                break;
+        }
+        switch (PlayerPrefs.GetInt("Mprev", 0))
+        {
+            case 1:
+                Botones[3].SetActive(false);
+                break;
+            case 2:
+                Botones[3].SetActive(false);
+                Botones[4].SetActive(false);
+                break;
+            case 3:
+                Botones[3].SetActive(false);
+                Botones[4].SetActive(false);
+                Botones[5].SetActive(false);
+                break;
+        }
+
+    }
     public Slider sliderInfra, sliderAmbi, sliderPrev;
     private void Start()
     {
+        //mejorar el rendimiento del juego
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = -1;
+
+        //testeo
         IniciarSlider();
+        //buscarlucas
         ML=FindObjectOfType<MonedasLucas>();
     }
     void Update()
@@ -21,83 +78,83 @@ public class MejorasTienda : MonoBehaviour
         {
 
         }
-        Debug.Log("INFRA"+Infra);
-        Debug.Log("Ambi" + Ambi);
-        Debug.Log("Preve" + Prev);
+        Debug.Log("INFRA " + PlayerPrefs.GetInt("Minfra", 0));
+        Debug.Log("Ambi" + PlayerPrefs.GetInt("Mambi", 0));
+        Debug.Log("Preve" + PlayerPrefs.GetInt("Mprev", 0));
     }
     public void Infraestructura1()
     {
-        if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo1 && Infra == 0)
+        if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo1 && PlayerPrefs.GetInt("Minfra", 0) == 0)
         {
             ML.RestarLucas(Costo1);
-            Infra += 1;
+            PlayerPrefs.SetInt("Minfra", 1);
             Botones[0].SetActive(false);
         }
     }
     public void Infraestructura2()
-    { if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo2 && Infra == 1)
+    { if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo2 && PlayerPrefs.GetInt("Minfra", 0) == 1)
         {
-            ML.RestarLucas(Costo2);
-            Infra += 1;
+            ML.RestarLucas(Costo2); 
+            PlayerPrefs.SetInt("Minfra", 2);
             Botones[1].SetActive(false);
         } }
     public void Infraestructura3() 
     { 
-        if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo3 && Infra == 2)
+        if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo3 && PlayerPrefs.GetInt("Minfra", 0) == 2)
         {
             ML.RestarLucas(Costo3);
-            Infra += 1;
+            PlayerPrefs.SetInt("Minfra", 3);
             Botones[2].SetActive(false);
         }
      }
 
     public void Ambientación1()
     {
-        if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo1 && Ambi == 0)
+        if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo1 && PlayerPrefs.GetInt("Mambi", 0) == 0)
         {
             ML.RestarLucas(Costo1);
-            Ambi += 1;
+            PlayerPrefs.SetInt("Mambi", 1);
             Botones[6].SetActive(false);
         } }
     public void Ambientación2() {
-        if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo2 && Ambi == 1)
+        if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo2 && PlayerPrefs.GetInt("Mambi", 0) == 1)
         {
             ML.RestarLucas(Costo2);
-            Ambi += 1;
+            PlayerPrefs.SetInt("Mambi", 2);
             Botones[7].SetActive(false);
         } }
     public void Ambientación3() { 
-        if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo3 && Ambi == 2)
+        if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo3 && PlayerPrefs.GetInt("Mambi", 0) == 2)
         {
             ML.RestarLucas(Costo3);
-            Ambi += 1;
+            PlayerPrefs.SetInt("Mambi", 3);
             Botones[8].SetActive(false);
         }
     }
     public void Prevención1()
     {
-        if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo1 && Prev == 0)
+        if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo1 && PlayerPrefs.GetInt("Mprev", 0) == 0)
         {
             ML.RestarLucas(Costo1);
-            Prev += 1;
+            PlayerPrefs.SetInt("Mprev", 1);
             Botones[3].SetActive(false);
 
         } }
     public void Prevención2()
     {
-        if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo2 && Prev == 1)
+        if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo2 && PlayerPrefs.GetInt("Mprev", 0) == 1)
         {
             ML.RestarLucas(Costo2);
-            Prev += 1;
+            PlayerPrefs.SetInt("Mprev", 2);
             Botones[4].SetActive(false);
         }
     }
     public void Prevención3() {
             
-        if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo3 && Prev == 2)
+        if (PlayerPrefs.GetInt("Puntaje Maximo", 0) > Costo3 && PlayerPrefs.GetInt("Mprev", 0) == 2)
         {
             ML.RestarLucas(Costo3);
-            Prev += 1;
+            PlayerPrefs.SetInt("Mprev", 3);
             Botones[5].SetActive(false);
         }
     }
@@ -107,10 +164,7 @@ public class MejorasTienda : MonoBehaviour
         sliderPrev = GameObject.FindGameObjectWithTag("SliderAmbi").GetComponent<Slider>();
         sliderAmbi = GameObject.FindGameObjectWithTag("SliderPrev").GetComponent<Slider>();*/
         sliderInfra.maxValue = MaximoV;
-        sliderInfra.value = Infra;
         sliderAmbi.maxValue = MaximoV;
-        sliderAmbi.value = Ambi;
         sliderPrev.maxValue = MaximoV;
-        sliderPrev.value = Prev;
     }
 }
