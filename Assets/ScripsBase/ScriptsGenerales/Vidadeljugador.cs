65e5dbc9747037f4f94df47637ac2d197282c84f
JorgeL-F-M <jorgeluisflorezmendoza@gmail.com>
2023-06-29 23:46:43 -0500

    Jorge Florez- Para q la vida figure en hud
    Aun falta poner el modelo pero ahi puse en el mismo scripte de vida  para q se cree una vida adicional maximo hasta tres vidas

--- Assets/ScripsBase/ScriptsGenerales/Vidadeljugador.cs
+++ Assets/ScripsBase/ScriptsGenerales/Vidadeljugador.cs
@@ -7,6 +7,9 @@ public class Vidadeljugador : MonoBehaviour
     private float vida = 2;
     private bool invencible = false;
     private float tiempodeinvencible = 1f;
+    public GameObject life1;
+    public GameObject life2;
+    public GameObject life3;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,14 +22,40 @@ public class Vidadeljugador : MonoBehaviour
         
     }
 
-    private void menosvida (int cantidad)
+    public void ChangeLife(int value)
+    {
+        life += value;
+
+        if (life <= 0)
+        {
+            clip.Play();
+            Destroy(GetComponent<SpriteRenderer>());
+            Destroy(GetComponent<Collider2D>());
+            defeat.SetActive(true);
+            Destroy(gameObject, 1f);
+            life1.SetActive(false);
+            Time.timeScale = 0f;
+        }
+
+        else if (life <= 1)
+        {
+            life2.SetActive(false);
+        }
+
+        else if (life <= 2)
+        {
+            life3.SetActive(false);
+        }
+    }
+    public void menosvida (int cantidad)
     {
         if (!invencible && vida > 0)
         {
             vida -= cantidad;
             StartCoroutine(inmortal());
         }
-            
+        
+
         IEnumerator inmortal()
         {
             invencible = true;
@@ -34,6 +63,29 @@ public class Vidadeljugador : MonoBehaviour
             invencible = false;
         }
 
+        
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Obstaculo") && invencible == false)
+        {
+            Vida -= 1;
 
+        }
+        if (other.gameObject.CompareTag("Civil") && invencible == false)
+        {
+            Vida += 1;
+            ChangeLife(+1);
+
+            if (life == 2)
+            {
+                life2.SetActive(true);
+            }
+            else if (life == 3)
+            {
+                life3.SetActive(true);
+            }
+
+        }
+    }
 }
