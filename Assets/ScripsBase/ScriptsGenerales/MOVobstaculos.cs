73da3e2985d29ff499cfc4975a3d4b105a1ca377
JuanJoséFerreiraChavez <jjtrabajos159@gmail.com>
2023-07-07 04:02:39 -0500

    EfectosVisualesdeInterface
    se añade animaciones y modelos para el UI.
    Re ajuste de la tienda Implementación
    de más ui y fondo para menú

--- Assets/ScripsBase/ScriptsGenerales/MOVobstaculos.cs
+++ Assets/ScripsBase/ScriptsGenerales/MOVobstaculos.cs
@@ -7,27 +7,17 @@ public class MOVobstaculos : MonoBehaviour
     Rigidbody rb;
     private float NumRandom;
     GameManager Gm;
-    ManagerVariante GVM;
-    bool T;
+   
     
     void Start()
     {
-        if (T == false)
-        {
             Gm = FindObjectOfType<GameManager>();
             NumRandom = Random.Range(Gm.VelocidadGeneral - 25, Gm.VelocidadGeneral + 24);
             rb = GetComponent<Rigidbody>();
             rb.velocity -= new Vector3(0, 0, NumRandom);
             // Debug.Log(NumRandom);
-        }
-        else
-        {    GVM= FindObjectOfType<ManagerVariante>();
-            NumRandom = Random.Range(Gm.VelocidadGeneral - 25, Gm.VelocidadGeneral + 24);
-            rb = GetComponent<Rigidbody>();
-            rb.velocity -= new Vector3(0, 0, NumRandom);
-        }
-       
-    }
+            
+    }   
 
     void Update()
     {
